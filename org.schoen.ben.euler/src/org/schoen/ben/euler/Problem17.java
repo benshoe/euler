2ffe1de3f4e9e0cd89dc5f2c39aa2b562bf4170a
Ben Schoen <ben.schoen@itris.nl>
2014-10-13 22:38:49 +0200

    Problem 17 solved.

--- org.schoen.ben.euler/src/org/schoen/ben/euler/Problem17.java
+++ org.schoen.ben.euler/src/org/schoen/ben/euler/Problem17.java
@@ -10,7 +10,7 @@ public class Problem17 implements IEulerProblem {
 		SIXTEEN(7), SEVENTEEN(9), EIGHTEEN(8), NINETEEN(8), TWENTY(6), //
 		THIRTY(6), FORTY(5), FIFTY(5), SIXTY(5), //
 		SEVENTY(7), EIGHTY(6), NINETY(6), HUNDRED(7), //
-		THOUSAND(8);
+		ONE_THOUSAND(11), NONE(0);
 
 		private int m_length;
 
@@ -23,76 +23,94 @@ public class Problem17 implements IEulerProblem {
 		}
 	};
 
-	private static Map<String, Integer> m_numberMap = new HashMap<>();
+	private static Map<String, NUMBERS> m_numberMap = new HashMap<>();
 
 	static {
-		m_numberMap.put("0", new Integer(0));
-		m_numberMap.put("00", new Integer(0));
-		m_numberMap.put("1", new Integer(3)); //ONE
-		m_numberMap.put("2", new Integer(3)); //TWO
-		m_numberMap.put("3", new Integer(5)); //THREE
-		m_numberMap.put("4", new Integer(4)); //FOUR
-		m_numberMap.put("5", new Integer(4)); //FIVE
-		m_numberMap.put("6", new Integer(3)); //SIX
-		m_numberMap.put("7", new Integer(5)); //SEVEN
-		m_numberMap.put("8", new Integer(5)); //EIGHT
-		m_numberMap.put("9", new Integer(4)); //NINE
-		m_numberMap.put("10", new Integer(3)); //TEN
-		m_numberMap.put("11", new Integer(6)); //ELEVEN
-		m_numberMap.put("12", new Integer(6)); //TWELVE
-		m_numberMap.put("13", new Integer(8)); //THIRTEEN
-		m_numberMap.put("14", new Integer(8)); //FOURTEEN
-		m_numberMap.put("15", new Integer(7)); //FIFTEEN
-		m_numberMap.put("16", new Integer(7)); //SIXTEEN
-		m_numberMap.put("17", new Integer(9)); //SEVENTEEN
-		m_numberMap.put("18", new Integer(8)); //EIGHTEEN
-		m_numberMap.put("19", new Integer(8)); //NINETEEN
-		m_numberMap.put("20", new Integer(6)); //TWENTY
-		m_numberMap.put("30", new Integer(6)); //THIRTY
-		m_numberMap.put("40", new Integer(5)); //FORTY
-		m_numberMap.put("50", new Integer(5)); //FIFTY
-		m_numberMap.put("60", new Integer(5)); //SIXTY
-		m_numberMap.put("70", new Integer(7)); //SEVENTY
-		m_numberMap.put("80", new Integer(6)); //EIGHTY
-		m_numberMap.put("90", new Integer(6)); //NINETY
-		m_numberMap.put("1000", new Integer(8)); //THOUSAND
+		m_numberMap.put("0", NUMBERS.NONE);
+		m_numberMap.put("00", NUMBERS.NONE);
+		m_numberMap.put("1", NUMBERS.ONE); //ONE
+		m_numberMap.put("2", NUMBERS.TWO); //TWO
+		m_numberMap.put("3", NUMBERS.THREE); //THREE
+		m_numberMap.put("4", NUMBERS.FOUR); //FOUR
+		m_numberMap.put("5", NUMBERS.FIVE); //FIVE
+		m_numberMap.put("6", NUMBERS.SIX); //SIX
+		m_numberMap.put("7", NUMBERS.SEVEN); //SEVEN
+		m_numberMap.put("8", NUMBERS.EIGHT); //EIGHT
+		m_numberMap.put("9", NUMBERS.NINE); //NINE
+		m_numberMap.put("10", NUMBERS.TEN); //TEN
+		m_numberMap.put("11", NUMBERS.ELEVEN); //ELEVEN
+		m_numberMap.put("12", NUMBERS.TWELVE); //TWELVE
+		m_numberMap.put("13", NUMBERS.THIRTEEN); //THIRTEEN
+		m_numberMap.put("14", NUMBERS.FOURTEEN); //FOURTEEN
+		m_numberMap.put("15", NUMBERS.FIFTEEN); //FIFTEEN
+		m_numberMap.put("16", NUMBERS.SIXTEEN); //SIXTEEN
+		m_numberMap.put("17", NUMBERS.SEVENTEEN); //SEVENTEEN
+		m_numberMap.put("18", NUMBERS.EIGHTEEN); //EIGHTEEN
+		m_numberMap.put("19", NUMBERS.NINETEEN); //NINETEEN
+		m_numberMap.put("20", NUMBERS.TWENTY); //TWENTY
+		m_numberMap.put("30", NUMBERS.THIRTY); //THIRTY
+		m_numberMap.put("40", NUMBERS.FORTY); //FORTY
+		m_numberMap.put("50", NUMBERS.FIFTY); //FIFTY
+		m_numberMap.put("60", NUMBERS.SIXTY); //SIXTY
+		m_numberMap.put("70", NUMBERS.SEVENTY); //SEVENTY
+		m_numberMap.put("80", NUMBERS.EIGHTY); //EIGHTY
+		m_numberMap.put("90", NUMBERS.NINETY); //NINETY
+		m_numberMap.put("100", NUMBERS.HUNDRED); //HUNDRED
+		m_numberMap.put("1000", NUMBERS.ONE_THOUSAND); //THOUSAND
 
 	}
+
+	private String m_answer;
 
 	@Override
 	public void run() {
 		int sum = 0;
 		for(int i = 1; i <= 1000; i++) {
+			System.out.println(" " + sum);
+			if(i == 100) {
+				sum += 10;
+				continue;
+			}
 			if(m_numberMap.containsKey(String.valueOf(i))) {
-				sum += m_numberMap.get(String.valueOf(i)).intValue();
+				sum += m_numberMap.get(String.valueOf(i)).getLength();
+				System.out.print(m_numberMap.get(String.valueOf(i)) + " ");
 				continue;
 			}
 
 			if(i < 100) {
 				String s = String.valueOf(i);
-				sum += m_numberMap.get(s.substring(0, 1) + "0").intValue();
-				sum += m_numberMap.get(s.substring(1)).intValue();
+				sum += m_numberMap.get(s.substring(0, 1) + "0").getLength();
+				System.out.print(m_numberMap.get(s.substring(0, 1) + "0") + " ");
+				sum += m_numberMap.get(s.substring(1)).getLength();
+				System.out.print(m_numberMap.get(s.substring(1)) + " ");
 				continue;
 			}
 
 			if(i < 1000) {
 				String s = String.valueOf(i);
-				sum += m_numberMap.get(s.substring(0, 1)).intValue();
-				sum += 7; //HUNDRED
+				sum += m_numberMap.get(s.substring(0, 1)).getLength();
+				System.out.print(m_numberMap.get(s.substring(0, 1)) + " ");
+				sum += m_numberMap.get("100").getLength(); //HUNDRED
+				System.out.print(m_numberMap.get("100") + " ");
 				if(i % 100 == 0) {
 					//We're done because this is a round hundred value
 					continue;
 				}
 				sum += 3; // AND as in TWO HUNDRED AND FIFTY ONE
+				System.out.print("AND ");
 				if(m_numberMap.containsKey(s.substring(1, 3))) {
-					sum += m_numberMap.get(s.substring(1, 3)).intValue();
+					sum += m_numberMap.get(s.substring(1, 3)).getLength();
+					System.out.print(m_numberMap.get(s.substring(1, 3)) + " ");
 					continue;
 				}
-				sum += m_numberMap.get(s.substring(1, 2) + "0").intValue();
-				sum += m_numberMap.get(s.substring(2)).intValue();
+				sum += m_numberMap.get(s.substring(1, 2) + "0").getLength();
+				System.out.print(m_numberMap.get(s.substring(1, 2) + "0") + " ");
+				sum += m_numberMap.get(s.substring(2)).getLength();
+				System.out.print(m_numberMap.get(s.substring(2)));
 			}
 		}
 		System.out.println(sum);
+		m_answer = String.valueOf(sum);
 
 	}
 
@@ -103,7 +121,7 @@ public class Problem17 implements IEulerProblem {
 
 	@Override
 	public boolean isCorrectAnswer() {
-		return false;
+		return "21124".equals(m_answer);
 	}
 
 }
