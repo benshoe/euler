29f22ca368ad1857c047f414b8653508caef155e
Ben Schoen <ben.schoen@itris.nl>
2014-10-18 14:58:10 +0200

    Compile error solved.

--- org.schoen.ben.euler/src/org/schoen/ben/euler/EulerRunner.java
+++ org.schoen.ben.euler/src/org/schoen/ben/euler/EulerRunner.java
@@ -16,22 +16,13 @@ public class EulerRunner {
 		//		m_problems.add(new Opdracht9());
 		//		m_problems.add(new Opdracht10());
 		//		m_problems.add(new Opdracht11());
-<<<<<<< HEAD
-		//		m_problems.add(new Problem12());
-		//		m_problems.add(new Problem13());
-		//		m_problems.add(new Problem14());
-		//		m_problems.add(new Problem15());
-		//		m_problems.add(new Problem16());
-		//		m_problems.add(new Problem17());
-		m_problems.add(new Problem18());
-=======
 		m_problems.add(new Problem12());
 		m_problems.add(new Problem13());
 		m_problems.add(new Problem14());
 		m_problems.add(new Problem15());
 		m_problems.add(new Problem16());
 		m_problems.add(new Problem17());
->>>>>>> 3a53700c580d3e687703aebf0f6ddb641e230047
+		m_problems.add(new Problem18());
 	}
 
 	public static void main(String[] args) {
