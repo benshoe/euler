e65ac9414a0f11f3b8b475f635b1127ad96dd643
Ben Schoen <ben.schoen@itris.nl>
2014-11-06 19:07:14 +0100

    Palindrome aangepast.

--- org.schoen.ben.euler/src/org/schoen/ben/euler/util/MathUtil.java
+++ org.schoen.ben.euler/src/org/schoen/ben/euler/util/MathUtil.java
@@ -19,11 +19,7 @@ public class MathUtil {
 
 	public static boolean isPalindrome(long startNumber) {
 		String palindrome = String.valueOf(startNumber);
-		StringBuilder sb = new StringBuilder(palindrome);
-		if(palindrome.equals(sb.reverse().toString())) {
-			return true;
-		}
-		return false;
+		return isPalindrome(palindrome);
 	}
 
 	public static boolean isPalindrome(String s) {
