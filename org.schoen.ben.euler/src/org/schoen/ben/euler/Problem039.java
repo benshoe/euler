9578e51c816e55d43156ce1f2c540237041b8772
Ben Schoen <ben.schoen@itris.nl>
2014-11-07 20:40:51 +0100

    Probleem 39 opgelost, maar traag.

--- org.schoen.ben.euler/src/org/schoen/ben/euler/Problem039.java
+++ org.schoen.ben.euler/src/org/schoen/ben/euler/Problem039.java
@@ -1,5 +1,8 @@
 package org.schoen.ben.euler;
 
+import java.util.*;
+import java.util.Map.Entry;
+
 /**
  * If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
  *
@@ -19,8 +22,35 @@ public class Problem039 extends AbstractEulerProblem {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		Map<Integer, Integer> triangles = new HashMap<>(1000);
+		for(int i = 3; i <= 1000; i++) {
+			triangles.put(new Integer(i), new Integer(0));
+		}
+		for(int a = 1; a < 400; a++) {
+			for(int b = 1; b < 400; b++) {
+				for(int c = 1; c < 400; c++) {
+					if(a + b + c > 1000)
+						break;
+					if(Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) {
+						Integer key = new Integer(a + b + c);
+						Integer numberFound = triangles.get(key);
+						triangles.put(key, new Integer(numberFound.intValue() + 1));
+					}
+				}
+			}
+		}
+		Set<Entry<Integer, Integer>> entrySet = triangles.entrySet();
+		int maxTriangles = 0;
+		int triangle = 0;
+		for(Entry<Integer, Integer> entry : entrySet) {
+			if(entry.getValue().intValue() > maxTriangles) {
+				maxTriangles = entry.getValue().intValue();
+				triangle = entry.getKey().intValue();
+				System.out.println(triangle + ": " + maxTriangles);
+			}
 
+		}
+		setAnswer(String.valueOf(triangle));
 	}
 
 	@Override
