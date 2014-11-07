81ef659d30f6d4025a0ece9c98386da2194250dd
Ben Schoen <ben.schoen@itris.nl>
2014-11-07 11:24:33 +0100

    Gestart met probleem 39

---
+++ org.schoen.ben.euler/src/org/schoen/ben/euler/Problem039.java
@@ -0,0 +1,31 @@
+package org.schoen.ben.euler;
+
+/**
+ * If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
+ *
+ * {20,48,52}, {24,45,51}, {30,40,50}
+ *
+ * For which value of p ≤ 1000, is the number of solutions maximised?
+ *
+ *
+ * @author <a href="mailto:ben.schoen@itris.nl">Ben Schoen</a>
+ * @since Nov 7, 2014
+ */
+public class Problem039 extends AbstractEulerProblem {
+
+	public Problem039(String answer) {
+		super(answer);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getProblemName() {
+		return "Problem 39: For which value of p ≤ 1000, is the number of solutions maximised?";
+	}
+
+}
