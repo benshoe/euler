9f28c765e02ef5a8f92d85ab28ffd02aab5acfac
Ben Schoen <ben.schoen@itris.nl>
2014-11-08 22:55:09 +0100

    Gestart met probleem 44.

--- org.schoen.ben.euler/src/org/schoen/ben/euler/EulerRunner.java
+++ org.schoen.ben.euler/src/org/schoen/ben/euler/EulerRunner.java
@@ -47,7 +47,8 @@ public class EulerRunner {
 		//		m_problems.add(new Problem040("210"));
 		//		m_problems.add(new Problem041("7652413"));
 		//		m_problems.add(new Problem042("162"));
-		m_problems.add(new Problem043("16695334890"));
+		//		m_problems.add(new Problem043("16695334890"));
+		m_problems.add(new Problem044(""));
 	}
 
 	public static void main(String[] args) {
