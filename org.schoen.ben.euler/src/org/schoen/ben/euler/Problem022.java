367c0cf871ee09c5414ae30d1be3399e017f3554
Ben Schoen <ben.schoen@itris.nl>
2014-11-08 16:55:01 +0100

    javadoc gewijzigd in probleem 22

--- org.schoen.ben.euler/src/org/schoen/ben/euler/Problem022.java
+++ org.schoen.ben.euler/src/org/schoen/ben/euler/Problem022.java
@@ -7,7 +7,9 @@ import java.util.stream.*;
 import org.schoen.ben.euler.util.*;
 
 /**
- * For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+ * For example, when the list is sorted into alphabetical order, COLIN,
+ * which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
+ * So, COLIN would obtain a score of 938 × 53 = 49714.
  *
  * What is the total of all the name scores in the file?
  *
